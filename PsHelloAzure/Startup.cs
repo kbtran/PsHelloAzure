ab273b4a06c530544c4a91770e6243ad652f6e32
Bruce Tran <bruce.tran@gmail.com>
2020-04-18 20:45:08 -0400

    Add Application Insights Telemetry

--- PsHelloAzure/Startup.cs
+++ PsHelloAzure/Startup.cs
@@ -34,6 +34,7 @@ namespace PsHelloAzure
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
