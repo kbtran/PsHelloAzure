d695dfe6691826ddacbbec1c30117fee1ad32e8b
Bruce Tran <bruce.tran@gmail.com>
2020-04-18 20:09:44 -0400

    Update home controller for custom greeting.

--- PsHelloAzure/Controllers/HomeController.cs
+++ PsHelloAzure/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using PsHelloAzure.Models;
 
@@ -12,15 +13,18 @@ namespace PsHelloAzure.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = _configuration["Greeting"];
+            return View("Index", model);
         }
 
         public IActionResult Privacy()
